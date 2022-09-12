0ed6d4e60b3200b623a32baf82736784aa5b2b40
Nicolai <nicolai_m404@hotmail.com>
2022-09-12 20:16:34 +0200

    init next

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>nicmic</title>
+        <meta name="description" content="nicmic music" />
+        <link rel="icon" href="" />
+      </Head>
+
+      <main>
+       nicmic
+      </main>
+      <footer>
+       
+      </footer>
+    </div>
+  )
+}
+
+export default Home
