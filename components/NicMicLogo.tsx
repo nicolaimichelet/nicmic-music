c08f2c933bb1cef9d46d65ef524d3c030eaff45f
Nicolai <nicolai_m404@hotmail.com>
2022-09-20 21:49:34 +0200

    fix layout, footer, logo, hover color

---
+++ components/NicMicLogo.tsx
@@ -0,0 +1,8 @@
+const NicMicLogo = ({twClassName = "h-64"}: {twClassName?: string}) => (
+  <svg className={`${twClassName}`} viewBox="0 0 177 254" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M87.7073 245.073C87.7073 115.073 141 -30.5 174 31.5" stroke="white" stroke-width="4.5" stroke-linecap="round"/>
+    <path d="M87.7073 251.073L77.5 67.5C77.5 130.5 -21.5 214.5 15.5 147.5" stroke="white" stroke-width="4.5" stroke-linecap="round"/>
+  </svg>
+)
+
+export default NicMicLogo
